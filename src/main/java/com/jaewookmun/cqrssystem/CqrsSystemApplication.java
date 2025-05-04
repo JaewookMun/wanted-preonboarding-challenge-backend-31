bc7400c620b9d8a666d4050c1040ef3cd0bf7984
JaewookMun <mun1103.dev@gmail.com>
2025-05-04 13:31:41 +0900

    feature: add springboot project
    - add springboot project
    - refactor sql file and guids

---
+++ src/main/java/com/jaewookmun/cqrssystem/CqrsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.jaewookmun.cqrssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CqrsSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaewookmun/cqrssystem/CqrsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaewookmun.cqrssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
